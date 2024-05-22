d30eabf1fe383a25fe8b7aa5d1dfcb3ca6071deb
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-22 16:26:45 +0530

    added hide unhide on divs according to need

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 const searchBtn = document.getElementById("search-btn");
 const CurrentLocationBtn = document.getElementById("current-location-btn");
 // const apiKey = "8f6200216e7a219e044fb1179fea87b6";
+const hideCurrent = document.querySelector(".hideCurrent");
+const hideAll = document.querySelector(".hideAll");
 
 searchBtn.addEventListener("click", function () {
   const city = document.getElementById("city-input").value.trim();
   fetchWeather(city);
+  hideCurrent.classList.remove("hideCurrent");
+  hideAll.classList.remove("hideAll");
 });
 
 CurrentLocationBtn.addEventListener("click", function () {
