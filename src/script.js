d13c6474383de56422dbb88cc2d967cd85da21af
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-22 15:19:05 +0530

    added a event listener on search button

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
-const city = document.getElementById("city-input").value.trim();
 const searchBtn = document.getElementById("search-btn");
 const CurrentLocationBtn = document.getElementById("current-location-btn");
 const apiKey = "8f6200216e7a219e044fb1179fea87b6";
+
+searchBtn.addEventListener("click", function () {
+  const city = document.getElementById("city-input").value.trim();
+  console.log(city);
+});
