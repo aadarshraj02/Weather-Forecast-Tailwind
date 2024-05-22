a98a7845a1e5318f64697c07da3c689ea67b60a5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-22 15:12:05 +0530

    selected input,searchbtn and current location button

---
+++ src/script.js
@@ -0,0 +1,3 @@
+const city = document.getElementById("city-input").value.trim();
+const searchBtn = document.getElementById("search-btn");
+const CurrentLocationBtn = document.getElementById("current-location-btn");
