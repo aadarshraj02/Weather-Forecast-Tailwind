053f4c31ab45019b810770f62ff5a07ef5ee0a97
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-22 15:12:46 +0530

    added api key

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
 const city = document.getElementById("city-input").value.trim();
 const searchBtn = document.getElementById("search-btn");
 const CurrentLocationBtn = document.getElementById("current-location-btn");
+const apiKey = "8f6200216e7a219e044fb1179fea87b6";
